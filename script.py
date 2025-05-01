8368addf14ac6d2eb3d3c81dfca139e911751867
hasini2607 <jevajihasini@gmail.com>
2025-05-01 15:04:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,18 +1,5 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+# script.py
 
-\f0\fs24 \cf0 \
-# script.py\
-\
-name = "Javajihasini"\
-roll_number = \'93se22ucse114\'94\
-\
-print(f"Name: \{javajihasini)\
-print(f"Roll Number: \{se22ucse114\}\'94)\
-\
-print("This is Job C executing a Python script from Git.")\
-}
+print("Javajihasini")
+print("se22ucse114")
+print("This is Jenkins Job C running a Python script.")
